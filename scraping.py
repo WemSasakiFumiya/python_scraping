1f0c46a98e41f13eab5a8414983bf7c0f695a5ed
SasakiFumiya <sasaki.fumiya@vv-e-nn.com>
2023-05-25 16:51:21 +0900

    change url for scraping

--- scraping.py
+++ scraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def get_scraping_data():
-    response = requests.get('https://member.digskill.net/trial')
+    response = requests.get('https://tabelog.com')
     soup = BeautifulSoup(response.content, 'html.parser')
     title = soup.find('title').text
     return title
