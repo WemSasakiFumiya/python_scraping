cf8193ccaa94e1191a2300c02461f3924819340f
SasakiFumiya <sasaki.fumiya@vv-e-nn.com>
2023-05-30 17:54:09 +0900

    divided into functions

--- scraping_earthquake.py
+++ scraping_earthquake.py
@@ -29,6 +29,32 @@ def get_gss_workbook():
     return gs.open_by_key(key)
 
 
+def create_driver(url):
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    return driver
+
+
+def get_elements(seleniums, tag_name):
+    return seleniums.find_elements(By.TAG_NAME, tag_name)
+
+
+def create_condition_number(content, filter_field):
+    return float(content) if filter_field == 'マグニチュード' else int(
+        content[2]) if content != '-' else 0
+
+
+def get_url(selenium):
+    return selenium.find_element(
+        By.TAG_NAME, 'a').get_attribute('href')
+
+
+def create_coordinate(contents):
+    latitude = '.'.join(re.findall(r"\d+", contents[1].text))
+    longitude = '.'.join(re.findall(r"\d+", contents[2].text))
+    return str(latitude) + ',' + str(longitude)
+
+
 options = Options()
 options.add_argument('--headless')
 options.add_argument("--no-sandbox")
@@ -45,37 +71,32 @@ class Scr():
         urls = []
         scraiping_data = [['地震検知日時', '緯度', '経度',
                            'マグニチュード', '震源の深さ', '震央地名', 'Google Map Url']]
-        driver = webdriver.Chrome(options=options)
-        driver.get('https://www.data.jma.go.jp/multi/quake/index.html?lang=jp')
+        driver = create_driver(
+            'https://www.data.jma.go.jp/multi/quake/index.html?lang=jp')
         time.sleep(1)
-        seleniums = driver.find_elements(By.TAG_NAME, 'tr')
+        seleniums = get_elements(driver, 'tr')
         titles = seleniums[0].text.split(' ')
         select_field_index = titles.index(self.filter_field)
-        for index, value in enumerate(seleniums[2:], 2):
-            seleniums_td = value.find_elements(By.TAG_NAME, "td")
+        for i, value in enumerate(seleniums[2:], 2):
+            seleniums_td = get_elements(value, 'td')
             contents = [value.text for value in seleniums_td]
             content = contents[select_field_index]
-            condition_number = float(content) if self.filter_field == 'マグニチュード' else int(
-                content[2]) if not content == '-' else 0
+            condition_number = create_condition_number(
+                content, self.filter_field)
             if condition_number >= self.filter_number:
-                url = seleniums_td[0].find_element(
-                    By.TAG_NAME, 'a').get_attribute('href')
+                url = get_url(seleniums_td[0])
                 urls.append(url)
         for url in urls:
             driver.get(url)
             time.sleep(1)
-            seleniums_detail_page = driver.find_elements(By.TAG_NAME, 'tr')
-            detail_contents = seleniums_detail_page[1].find_elements(
-                By.TAG_NAME, 'td')
-            latitude = '.'.join(re.findall(r"\d+", detail_contents[1].text))
-            longitude = '.'.join(re.findall(r"\d+", detail_contents[2].text))
-            lan_with_lon = str(latitude) + ',' + str(longitude)
+            seleniums_detail_page = get_elements(driver, 'tr')
+            detail_contents = get_elements(seleniums_detail_page[1], 'td')
+            coordinate = create_coordinate(detail_contents)
             google_map_url = 'https://maps.google.com/maps?ll=' + \
-                lan_with_lon + '&q=' + lan_with_lon + '&z=10'
+                coordinate + '&q=' + coordinate + '&z=10'
             detail_data = [content.text for content in detail_contents]
             detail_data.append(google_map_url)
             scraiping_data.append(detail_data)
-            print(scraiping_data)
         driver.quit()
         if len(scraiping_data) > 0:
             f = open('earthquake_info/' + self.file_name + '.txt', 'a')
@@ -90,5 +111,6 @@ class Scr():
                 'values': scraiping_data})
 
 
-sc = Scr('マグニチュード', 5, 'test')
+select_filter_field = ['マグニチュード', '最大震度']
+sc = Scr(select_filter_field[1], 2, 'max')
 sc.geturl()
